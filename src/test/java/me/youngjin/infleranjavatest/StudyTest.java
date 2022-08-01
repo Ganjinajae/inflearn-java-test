7fb646ac7b7e7483b3e210e972425f4af68fbad1
지영진 <youngjin.jee@nhnsoft.com>
2022-08-01 09:47:45 +0900

    feat: @DisplayName, @DisplayNameGeneration 실습

--- src/test/java/me/youngjin/infleranjavatest/StudyTest.java
+++ src/test/java/me/youngjin/infleranjavatest/StudyTest.java
@@ -4,18 +4,19 @@ import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
 class StudyTest  {
 
     @Test
-    void create() {
+    @DisplayName("스터디 만들기")
+    void create_new_study() {
         Study study = new Study();
         assertNotNull(study);
         System.out.println("create");
     }
 
     @Test
-    @Disabled("테스트")
-    void create1() {
+    void create_new_study_again() {
         System.out.println("create1");
         assertNotNull("테스트");
     }
