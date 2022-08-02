62191f940e489cd35daa44dd8dc9391682532cb3
지영진 <wldudwls12@gmail.com>
2022-08-02 14:21:13 +0900

    feat: mockito mock을 어노테이션으로 활용하는 방법

--- src/test/java/me/youngjin/infleranjavatest/study/StudyServiceTest.java
+++ src/test/java/me/youngjin/infleranjavatest/study/StudyServiceTest.java
@@ -2,16 +2,23 @@ package me.youngjin.infleranjavatest.study;
 
 import me.youngjin.infleranjavatest.member.MemberService;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.mock;
 
+@ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
+
+    @Mock
+    MemberService memberService;
+
+    @Mock
+    StudyRepository studyRepository;
 
     @Test
     void createStudyService() {
-        MemberService memberService = mock(MemberService.class);
-        StudyRepository studyRepository = mock(StudyRepository.class);
         StudyService studyService = new StudyService(memberService, studyRepository); // 구현체가 없는 경우 mock을 사용하기 좋다.
         assertNotNull(studyService);
     }
