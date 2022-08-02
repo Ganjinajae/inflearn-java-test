f306ca0881b15af89a6e2ab3569212d2d14ea2aa
지영진 <wldudwls12@gmail.com>
2022-08-02 14:46:51 +0900

    feat: stubbing 기본 실습

--- src/test/java/me/youngjin/infleranjavatest/domain/Study.java
+++ src/test/java/me/youngjin/infleranjavatest/domain/Study.java
@@ -1,6 +1,16 @@
 package me.youngjin.infleranjavatest.domain;
 
 public class Study {
+
+    private int limit;
+
+    private String name;
+
+    public Study(int limit, String name) {
+        this.limit = limit;
+        this.name = name;
+    }
+
     public void setOwner(Member member) {
     }
 }
--- src/test/java/me/youngjin/infleranjavatest/study/StudyServiceTest.java
+++ src/test/java/me/youngjin/infleranjavatest/study/StudyServiceTest.java
@@ -1,12 +1,17 @@
 package me.youngjin.infleranjavatest.study;
 
+import me.youngjin.infleranjavatest.domain.Member;
+import me.youngjin.infleranjavatest.domain.Study;
 import me.youngjin.infleranjavatest.member.MemberService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
@@ -18,9 +23,20 @@ class StudyServiceTest {
 //    StudyRepository studyRepository;
 
     @Test
-    void createStudyService(@Mock MemberService memberService, @Mock StudyRepository studyRepository) {
+    void createNewStudy(@Mock MemberService memberService, @Mock StudyRepository studyRepository) {
         StudyService studyService = new StudyService(memberService, studyRepository); // 구현체가 없는 경우 mock을 사용하기 좋다.
         assertNotNull(studyService);
+
+        Member member = new Member();
+        member.setId(1L);
+        member.setEmail("youngjinjee@email.com");
+        // 객체를 stubbing 하자
+        when(memberService.findById(1L)).thenReturn(Optional.of(member));
+
+        Study study = new Study(10, "java");
+
+//        studyService.createNewStudy(1L, study);
+        assertEquals("youngjinjee@email.com", memberService.findById(1L).get().getEmail());
     }
 
 }
