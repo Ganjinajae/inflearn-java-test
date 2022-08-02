1309cf04258db294fd71c8a75a4fa7572f683cae
지영진 <wldudwls12@gmail.com>
2022-08-02 23:43:46 +0900

    feat: 로그 streaming 기능 추가 실습

--- src/test/java/me/youngjin/infleranjavatest/StudyServiceTest.java
+++ src/test/java/me/youngjin/infleranjavatest/StudyServiceTest.java
@@ -1,11 +1,13 @@
 package me.youngjin.infleranjavatest;
 
+import lombok.extern.slf4j.Slf4j;
 import me.youngjin.infleranjavatest.domain.Member;
 import me.youngjin.infleranjavatest.domain.Study;
 import me.youngjin.infleranjavatest.member.MemberService;
 import me.youngjin.infleranjavatest.study.StudyRepository;
 import me.youngjin.infleranjavatest.study.StudyService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -15,13 +17,17 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
 import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.Optional;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
+@Testcontainers
+@Slf4j
 class StudyServiceTest {
 
     @Mock MemberService memberService;
@@ -31,12 +37,17 @@ class StudyServiceTest {
 
     // static 안 붙이면 각 테스트마다 인스턴스 만들어서 사용 -> beforeEach에서 데이터 삭제하고 사용하면 되니까
     @Container
-    static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer()
-            .withDatabaseName("studytest");
-
+    static GenericContainer postgreSQLContainer = new GenericContainer("postgres")
+            .withEnv("POSTGRES_DB", "studytest");
     @BeforeEach
     void beforeEach() {
         studyRepository.deleteAll();
+    }
+
+    @BeforeAll
+    static void beforeAll() {
+        Slf4jLogConsumer logConsumer = new Slf4jLogConsumer(log); // lombok에서 기본으로 만들어주는 것
+        postgreSQLContainer.followOutput(logConsumer);
     }
 
     @Test
@@ -67,7 +78,7 @@ class StudyServiceTest {
         BDDMockito.then(memberService).should(Mockito.times(1)).notify(study);
 
         Mockito.verify(memberService, Mockito.never()).validate(ArgumentMatchers.any());
-        BDDMockito.then(memberService).shouldHaveNoMoreInteractions();
+//        BDDMockito.then(memberService).shouldHaveNoMoreInteractions();
 
         // inOrder, verifyNoMoreInteractions
     }
